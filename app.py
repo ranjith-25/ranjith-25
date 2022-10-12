d147dbfac31ff124cfcef4e78d301ad3e803db50
ranjith-25 <113257591+ranjith-25@users.noreply.github.com>
2022-10-12 20:37:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import joblib
+
+model = joblib.load('spam-ham')# we are loading the pipelined model using joblib
+st.title('SPAM-HAM CLASSIFIER')#It creates a title in webapp
+ip = st.text_input('Enter the message')#It creates a text box in the webapp
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0]) #st.button will create a button with the name Predict
